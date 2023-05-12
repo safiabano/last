6869c83f511b500de1452c46b8201acb70623561
safiabano <safiabano736@gmail.com>
2023-05-12 22:45:44 +0500

    last deploy

---
+++ bootsgrid/assets/js/app.js
@@ -0,0 +1,10 @@
+//  var question = "Your species?";
+
+//  var spec = prompt(question,"human" );
+//  console.log();
+// var userInput =+ prompt("how many cats?");
+// var userCats =(userInput + 1);
+// console.log(userCats);
+// let userName = prompt("what is your name?");
+// let userOut = ("welcome " + userName);
+// console.log(userOut);
